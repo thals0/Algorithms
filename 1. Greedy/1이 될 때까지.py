8094fa261c2063e5d2248e5902f216baaaaffce1
SoMin <thals0107@naver.com>
2022-10-20 12:51:03 +0900

    Update

--- 1. Greedy/1이 될 때까지.py
+++ 1. Greedy/1이 될 때까지.py
@@ -1,33 +1,29 @@
-from tkinter.tix import Tree
-
-
 n, k = map(int, input().split())
 count = 0
 
 ## 단순 방법 답안
-# while n != 0:
-#   if n%k == 0:
-#     n = n/k
-#     count += 1
-#   else:
-#     n = n-1
-#     count += 1
+while n != 1:
+  if n%k == 0:
+    n = n//k
+    count += 1
+  else:
+    n = n-1
+    count += 1
 
-# 왜 이 방법으로 하면 1을 빼줘야 정답이 나올까 ?
-# print(count-1)
+print(count)
 
 ## 시간복잡도를 생각한 답안
-while True:
-  # (N==K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
-  target = (n//k)*k
-  count += (n-target)
-  n = target
+# while True:
+#   # (N==K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
+#   target = (n//k)*k
+#   count += (n-target)
+#   n = target
 
-  if n<k:
-    break
-  # K로 나누기
-  count += 1
-  n//=k
-# 마지막으로 남은 수에 대하여 1씩 빼기 
-count += (n-1)
-print(count)
+#   if n<k:
+#     break
+#   # K로 나누기
+#   count += 1
+#   n//=k
+# # 마지막으로 남은 수에 대하여 1씩 빼기 
+# count += (n-1)
+# print(count)
